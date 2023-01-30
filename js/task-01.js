720bbc51b8aaa436bd553cb2c43cdb8670382d09
JaneLev90 <jlev888@gmail.com>
2023-01-30 13:07:02 +0200

    task 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,4 +1,15 @@
-const categoriesList = document.querySelectorAll("li.item");
-console.log (`Number of categories:`);
+const categoriesList = document.querySelectorAll(".item");
+console.log (`Number of categories: `);
 console.log (categoriesList.length);
+
+const categoriesName = document.querySelectorAll('h2');
+console.log (`Category: ` );
+console.log (...categoriesName);
+
+
+
+
+
+
+
 

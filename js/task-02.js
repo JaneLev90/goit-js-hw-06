a6195153b8259abc5bffa1f66a0210182e55e9ad
JaneLev90 <jlev888@gmail.com>
2023-01-30 20:03:19 +0200

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientItems = ingredients.map(item => {
+  const ingredientItem = document.createElement("li");
+  ingredientItem.textContent = ingredients[0];
+  ingredientItem.classList.add('item');
+
+  return ingredientItem;
+  })
+  console.log(ingredientItems)
+const ingredientsList = document.querySelector('#ingredients');
+ingredientsList.appendChild(...ingredientItems);

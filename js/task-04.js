8579546dd15fbfec57d39a0707bcc9767b5b2acc
JaneLev90 <jlev888@gmail.com>
2023-02-03 22:20:22 +0200

    task 4

--- js/task-04.js
+++ js/task-04.js
@@ -1,16 +1,18 @@
-const decrementBtn = document.querySelector('.decrement');
-const incrementBtn = document.querySelector('.increment');
-const counter = document.querySelector('#value');
+
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+const incrementBtn = document.querySelector('[data-action="increment"]');
+let counterValue = document.querySelector('#value');
 
 
 incrementBtn.addEventListener('click', makePlus);
 decrementBtn.addEventListener('click', makeMinus);
 
-let counterValue = counter.textContent;
-console.log (counterValue);
+// counterValue.textContent = 5;
+// console.log (counterValue);
 
 function makePlus() {
-    console.log (counterValue.textContent += 1);
+   console.log('+')
+   
 };
 function makeMinus() {
     console.log('-')

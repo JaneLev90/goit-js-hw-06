ab96f002fa6af228b6e19777ebdaa624212a1ba9
JaneLev90 <jlev888@gmail.com>
2023-02-10 20:56:37 +0200

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,20 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+const text = document.querySelector('.color');
+const btn = document.querySelector('.change-color');
+
+btn.addEventListener('click', () =>{
+  // document.body.style.backgroundColor = Math.floor(Math.random() * 16777215)
+  //   .toString(16)
+  //   .padStart(6, 0);
+ document.body.style.background = Math.floor(Math.random() * 16777215)
+ .toString(16)
+ .padStart(6, 0);
+   text.textContent = `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+ 
+  });
+
+
+ 
+  
+
